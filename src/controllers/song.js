89f614f0757519c6c45542abae248bd43b16c2f5
iniadeniyi <inioluwa@ualberta.ca>
2024-06-01 23:28:35 -0600

    added pagination to the searchSongs function. 20 songs per page

--- src/controllers/song.js
+++ src/controllers/song.js
@@ -4,122 +4,112 @@ import { errorMsg, successMsg } from "../helpers/functions.js";
 import { Song } from "../models/Song.js";
 
 export const searchSongs = async (req, res) => {
-  try {
-    let songs = [];
-    const churchId = req.query.churchId;
+    try {
+        let songs = [];
+        const churchId = req.query.churchId;
+        const search = req.query.search;
+        const limit = 20;
+        const page = parseInt(req.query.page, 10) || 1;
+        const skip = (page - 1) * limit;
 
-    if (req.query.search) {
-      songs = await Song.aggregate([
-        {
-          $match: {
-            $and: [
-              { $text: { $search: req.query.search } },
-              {
-                $or: [{ isPublic: { $ne: false } }, { churchId }],
-              },
-            ],
-          },
-        },
-        {
-          $sort: { score: { $meta: "textScore" } },
-        },
-        {
-          $limit: 20,
-        },
-      ]);
-      res.json(successMsg(songs));
-    } else {
-      songs = await Song.aggregate([
-        {
-          $match: {
+        const matchQuery = {
             $or: [{ isPublic: { $ne: false } }, { churchId }],
-          },
-        },
-        { $sample: { size: 15 } },
-      ]);
-      res.json(successMsg(songs));
+        };
+
+        if (search) {
+            matchQuery.$and = [{ $text: { $search: search } }];
+        }
+
+        const aggregationPipeline = [
+            { $match: matchQuery },
+            search ? { $sort: { score: { $meta: "textScore" } } } : { $sample: { size: limit } },
+            { $skip: skip },
+            { $limit: limit },
+        ];
+
+        songs = await Song.aggregate(aggregationPipeline);
+        res.json(successMsg(songs));
+    } catch (err) {
+        console.error(`GET SONG ERROR: ${err}`);
+        res.status(400).json(errorMsg(err));
     }
-  } catch (err) {
-    console.error(`GET SONG ERROR: ${err}`);
-    res.status(400).json(errorMsg(err));
-  }
 };
 
 export const getSongsByChurch = async (req, res) => {
-  try {
-    const { churchId } = req.params;
-    const songs = await Song.find({ churchId });
+    try {
+        const { churchId } = req.params;
+        const songs = await Song.find({ churchId });
 
-    res.status(200).json(songs);
-  } catch (error) {
-    res.status(500).json({
-      message: "Error fetching songs",
-      error: error.message,
-    });
-  }
+        res.status(200).json(songs);
+    } catch (error) {
+        res.status(500).json({
+            message: "Error fetching songs",
+            error: error.message,
+        });
+    }
 };
 
 export const addSong = async (req, res) => {
-  try {
-    const userId = req.user._id;
-    const { churchId } = req.params;
+    try {
+        const userId = req.user._id;
+        const { churchId } = req.params;
 
-    const { id, title, artist, lyrics, isPublic } = req.body;
-    const newSong = await Song.create({
-      id,
-      title,
-      artist,
-      lyrics,
-      isPublic,
-      churchId,
-      createdBy: userId,
-    });
-    res.status(201).json(newSong);
-  } catch (error) {
-    res.status(500).json({
-      message: "Error creating song",
-      error: error.message,
-    });
-  }
+        const { id, title, artist, lyrics, isPublic } = req.body;
+        const newSong = await Song.create({
+            id,
+            title,
+            artist,
+            lyrics,
+            isPublic,
+            churchId,
+            createdBy: userId,
+        });
+        res.status(201).json(newSong);
+    } catch (error) {
+        res.status(500).json({
+            message: "Error creating song",
+            error: error.message,
+        });
+    }
 };
 
 export const updateSong = async (req, res) => {
-  const { songId } = req.params;
-  const songData = req.body;
+    const { songId } = req.params;
+    const songData = req.body;
 
-  try {
-    const updatedSong = await Song.findByIdAndUpdate(
-      songId,
-      { $set: songData },
-      { new: true, runValidators: true },
-    );
+    try {
+        const updatedSong = await Song.findByIdAndUpdate(
+            songId,
+            { $set: songData },
+            { new: true, runValidators: true },
+        );
 
-    if (!updatedSong) {
-      return res.status(404).json({ message: "Song not found" });
+        if (!updatedSong) {
+            return res.status(404).json({ message: "Song not found" });
+        }
+
+        res.status(200).json(updatedSong);
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to update song",
+            error: error.message,
+        });
     }
-
-    res.status(200).json(updatedSong);
-  } catch (error) {
-    res.status(500).json({
-      message: "Failed to update song",
-      error: error.message,
-    });
-  }
 };
 
 export const deleteSong = async (req, res) => {
-  try {
-    const { songId } = req.params;
+    try {
+        const { songId } = req.params;
 
-    const deletedSong = await Song.findByIdAndDelete(songId);
+        const deletedSong = await Song.findByIdAndDelete(songId);
 
-    if (!deletedSong) {
-      return res.status(404).json({ message: "Song not found" });
+        if (!deletedSong) {
+            return res.status(404).json({ message: "Song not found" });
+        }
+
+        res.status(200).json(successMsg(deletedSong));
+    } catch (error) {
+        console.error(`DELETE SONG ERROR: ${error}`);
+        res.status(500).json(errorMsg(error));
     }
-
-    res.status(200).json(successMsg(deletedSong));
-  } catch (error) {
-    console.error(`DELETE SONG ERROR: ${error}`);
-    res.status(500).json(errorMsg(error));
-  }
 };
