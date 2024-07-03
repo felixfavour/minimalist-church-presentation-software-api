eeda26dce7299e269d3cc44a3fe97ed19dbc4e6a
felixfavour <felixfavour0@gmail.com>
2024-07-03 15:17:01 +0100

    fix: double controller for single route

--- src/routes/song.js
+++ src/routes/song.js
@@ -1,12 +1,18 @@
 import express from "express";
 
-import { createSong, deleteSong, searchSongs, getSongsByChurch, updateSong } from "../controllers/song.js";
+import {
+  createSong,
+  deleteSong,
+  getSongsByChurch,
+  searchSongs,
+  updateSong,
+} from "../controllers/song.js";
 
 const router = express.Router();
 
 router.post("/", createSong);
-router.get("/", getSongsByChurch);
 router.get("/", searchSongs);
+router.get("/all", getSongsByChurch);
 router.put("/:songId", updateSong);
 router.delete("/:songId", deleteSong);
 
