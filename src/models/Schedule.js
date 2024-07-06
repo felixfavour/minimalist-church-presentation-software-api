df5ec517d979e2e1f79131d71dec36b286a74af4
felixfavour <felixfavour0@gmail.com>
2024-07-06 21:30:51 +0100

    chore: changes to schedules

--- src/models/Schedule.js
+++ src/models/Schedule.js
@@ -1,39 +1,35 @@
 import mongoose from "mongoose";
 
 const ScheduleSchema = new mongoose.Schema(
-    {
-        id: {
-            type: String,
-            required: true,
-        },
-        name: {
-            type: String,
-            required: true,
-        },
-        churchId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Church",
-            required: true,
-        },
-        authorId: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "User",
-            required: true,
-        },
-        editorIds: {
-            type: [mongoose.Schema.Types.ObjectId],
-            ref: "User",
-            required: true,
-        },
-        lastUpdated: {
-            type: Date,
-            required: true,
-            default: Date.now,
-        },
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        timestamps: true,
+    churchId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Church",
+      required: true,
     },
+    authorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    editorIds: {
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: "User",
+      required: true,
+    },
+    lastUpdated: {
+      type: Date,
+      required: true,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+  },
 );
 
 export const Schedule = mongoose.model("Schedule", ScheduleSchema);
