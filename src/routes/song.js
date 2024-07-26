2d82be7dbcd86c02b2bb50e64c060bfcdc6a5e2b
felixfavour <felixfavour0@gmail.com>
2024-07-26 10:13:58 +0100

    chore: added endpoint to get single song

--- src/routes/song.js
+++ src/routes/song.js
@@ -1,12 +1,20 @@
 import express from "express";
 
-import { createSong, deleteSong, getSongsByChurch, searchSongs, updateSong } from "../controllers/song.js";
+import {
+  createSong,
+  deleteSong,
+  getSong,
+  getSongsByChurch,
+  searchSongs,
+  updateSong,
+} from "../controllers/song.js";
 
 const router = express.Router();
 
 router.post("/", createSong);
 router.get("/", searchSongs);
 router.get("/all", getSongsByChurch);
+router.get("/:songId", getSong);
 router.put("/:songId", updateSong);
 router.delete("/:songId", deleteSong);
 
